f98dcff1922b9cc94e2f70a771a7c9db05cb89ce
Ajaz Ahamad <aahamad@neuvays.net>
2026-05-16 00:48:49 +0530

    updates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -35,6 +35,13 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./pages/contact/contact')
         .then(m => m.Contact)
+  },
+
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./pages/not-found/not-found')
+        .then(m => m.NotFound)
   }
 
 ];
